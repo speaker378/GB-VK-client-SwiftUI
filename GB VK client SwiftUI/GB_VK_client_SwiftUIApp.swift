545950120604e2e249f688d00df763a8606052f5
speaker378 <speaker378debua@gmail.com>
2022-03-13 20:52:43 +0500

    Initial Commit

---
+++ GB VK client SwiftUI/GB_VK_client_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GB_VK_client_SwiftUIApp.swift
+//  GB VK client SwiftUI
+//
+//  Created by Сергей Черных on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GB_VK_client_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
