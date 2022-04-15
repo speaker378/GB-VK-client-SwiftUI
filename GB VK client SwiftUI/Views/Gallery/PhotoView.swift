344c40a3a35cf31e93270e7f76a3ea2337d56b6f
speaker378 <speaker378debua@gmail.com>
2022-04-15 19:39:33 +0500

    LazyVGrid

--- GB VK client SwiftUI/Views/Gallery/PhotoView.swift
+++ GB VK client SwiftUI/Views/Gallery/PhotoView.swift
@@ -22,27 +22,30 @@ struct PhotoView: View, Identifiable {
     var url: URL?
     
     var body: some View {
-        ZStack {
-            AsyncImage(url: url) { image in
-                image
+        GeometryReader { geometry in
+            ZStack {
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width, height: geometry.size.width, alignment: .center)
+                        .clipped()
+                } placeholder: { ProgressView() }
+                
+                Image(systemName: "suit.heart.fill")
                     .resizable()
-                    .scaledToFit()
-            } placeholder: { ProgressView() }
-                .frame(width: 100 , height: 100)
-            
-            Image(systemName: "suit.heart.fill")
-                .resizable()
-                .frame(width: 24, height: 24)
-                .foregroundColor( flipped ? .red : .white)
-                .alignmentGuide(HorizontalAlignment.center, computeValue: {
-                    -$0.width })
-                .alignmentGuide(VerticalAlignment.center, computeValue: {
-                    -$0.height })
-                .rotation3DEffect((flipped ? Angle(degrees: 180): Angle(degrees: 0)), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
-                .animation(.default, value: flipped)
-                .onTapGesture {
-                    self.flipped.toggle()
-                }
+                    .frame(width: 24, height: 24)
+                    .foregroundColor( flipped ? .red : .white)
+                    .alignmentGuide(HorizontalAlignment.center, computeValue: {
+                        (-geometry.size.width/2 + $0.width) })
+                    .alignmentGuide(VerticalAlignment.center, computeValue: {
+                        (-geometry.size.height/2 + $0.height) })
+                    .rotation3DEffect((flipped ? Angle(degrees: 180): Angle(degrees: 0)), axis: (x: CGFloat(0), y: CGFloat(10), z: CGFloat(0)))
+                    .animation(.default, value: flipped)
+                    .onTapGesture {
+                        self.flipped.toggle()
+                    }
+            }
         }
     }
 }
@@ -51,5 +54,6 @@ struct PhotoView_Preview: PreviewProvider {
     static let photo = Photo(id: 0, ownerID: 0, sizes: [Size(urlString: "https://picsum.photos/200", width: 100, height: 100, type: .x)], likes: Likes(userLikes: 0, count: 21))
     static var previews: some View {
         PhotoView(photo: photo)
+            .frame(width: 200, height: 200, alignment: .center)
     }
 }
